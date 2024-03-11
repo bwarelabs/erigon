7335273bc887659ffe1bb6fc448d2bba6e97bc9a
Adrian Bacircea <adrian.bacircea@gmail.com>
2024-03-11 12:37:12 +0200

    Increase channel buffer size for eth_subscribe logs

--- turbo/jsonrpc/eth_filters.go
+++ turbo/jsonrpc/eth_filters.go
@@ -257,7 +257,7 @@ func (api *APIImpl) Logs(ctx context.Context, crit filters.FilterCriteria) (*rpc
 
 	go func() {
 		defer debug.LogPanic()
-		logs, id := api.filters.SubscribeLogs(128, crit)
+		logs, id := api.filters.SubscribeLogs(1024, crit)
 		defer api.filters.UnsubscribeLogs(id)
 
 		for {
